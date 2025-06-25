8bf699215c16847e8b65b457cf7c6dd1b7b82a5d
Sunny108700541 <sunnysinghchauhan082@gmail.com>
2025-06-25 23:46:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async" // ✅ import HelmetProvider
 import "./index.css"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <HelmetProvider> {/* wrap the app */}
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </HelmetProvider>
 )
